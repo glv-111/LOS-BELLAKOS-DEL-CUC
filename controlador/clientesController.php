1c757ca614c5e46c5cd3969f6fca4e54615cd561
Gabriel Glv <angelgm1004@gmail.com>
2024-01-25 09:10:06 -0600

    Hola

---
+++ controlador/clientesController.php
@@ -0,0 +1,13 @@
+<?php
+
+    switch($opc){
+
+        case 1: //Registrar comentarios de clientes
+
+        $nombre = $_POST['txtNombre'];
+        $correo = $_POST['txtCorreo'];
+        $telefono = $_POST['txtTelefono'];
+        $comentarios = $_POST['txtComentarios'];
+        //Registrar los valores en la base de datos
+    }
+?>
